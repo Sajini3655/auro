442a8213a1db0080584914d3410e5684598ab2d7
Sajini3655 <sajinitharushika3655@gmail.com>
2025-10-29 16:24:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"), // 👈 this is the one you installed
+  ],
+  plugins: [require("@tailwindcss/aspect-ratio")],
+
+}
